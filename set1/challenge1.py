c630bbb6f3574a2d143bec07d8d223369c9c9be4
rjdp <rjdp9736@gmail.com>
2018-06-27 09:19:44 +0530

    update challenge1 and add challenge2

--- set1/challenge1.py
+++ set1/challenge1.py
@@ -8,4 +8,4 @@ def hex_to_base64(hex_str):
 	>>> hex_to_base64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d')
 	'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 	"""
-	return base64.b64encode(codecs.decode(hex_str, 'hex'))
+	return base64.b64encode(codecs.decode(hex_str, 'hex')).decode("utf-8") 
